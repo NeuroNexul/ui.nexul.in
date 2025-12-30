1be7d3e4144f0290fcd01eb7cc8fbe49ce9c1b7d
Arif Sardar <arifsardar.private@gmail.com>
2025-12-30 22:29:04 +0530

    FEAT: Finished masonry row grid

--- registry/layouts/masonry/masonry-row.tsx
+++ registry/layouts/masonry/masonry-row.tsx
@@ -2,11 +2,16 @@
 
 import { cn } from "@/lib/utils";
 import React from "react";
-// import { calculateMasonryLayout } from "./revised";
-import { calculateMasonryLayout } from "./calculate-masonry";
+import {
+  calculateJustifiedMasonryLayout,
+  calculateNaiveMasonryLayout,
+} from "./calculate-masonry";
+import { mergeRefs } from "@/lib/merge-refs";
 
 export type GridItemProps = {
+  /** Original width of the grid item */
   width: number;
+  /** Original height of the grid item */
   height: number;
 
   // Additional properties can be added as needed
@@ -15,180 +20,146 @@ export type GridItemProps = {
 };
 
 export type Measures = {
+  /** Calculated width of the grid item */
   width: number;
+  /** Calculated height of the grid item */
   height: number;
+  /** Calculated top position of the grid item */
   top: number;
+  /** Calculated left position of the grid item */
   left: number;
 
+  /** Row number of the grid item in the row (0-based index) */
   index: number;
+  /** Row number of the grid item (1-based index) */
   rowNo: number;
+  /** Total items in the row */
   totalItemsInRow: number;
 };
 
-type Props = {
+export type Props = {
+  /** Container props for the masonry grid container */
   containerProps?: React.HTMLAttributes<HTMLDivElement>;
+  /** Item props for each masonry grid item */
   itemProps?: React.HTMLAttributes<HTMLDivElement>;
+  /** Gap between grid items */
   gap?: number;
+  /** Target height for each grid item; default is 300 */
   targetHeight?: number;
+  /** Scale factor for resizing grid items; default is 0.1 */
   scaleFactor?: number;
+  /** Layout type: "naive" or "justified"; default is "naive" */
+  layout?: "naive" | "justified";
+  /** Array of grid items to be displayed */
   items?: GridItemProps[];
+  /** Function to render each grid item */
   renderItem?: (
+    /** Props of the grid item to be rendered */
     item: GridItemProps,
+    /** Index of the grid item in the array */
     index: number,
+    /** Calculated measures for the grid item */
     measures: Measures
   ) => React.ReactNode;
 };
 
-export function MasonryRowGrid({
-  containerProps: { className, ...containerProps } = {},
-  itemProps: { className: itemClassName, style: itemStyle, ...itemProps } = {},
-  gap = 8,
-  targetHeight = 300,
-  scaleFactor = 0.1,
-  items = [],
-  renderItem,
-}: Props) {
-  const [viewportWidth, setViewportWidth] = React.useState<number>(0);
-  const containerRef = React.useRef<HTMLDivElement>(null);
+const MasonryRowGrid = React.forwardRef<HTMLDivElement, Props>(
+  (
+    {
+      containerProps: { className, ...containerProps } = {},
+      itemProps: {
+        className: itemClassName,
+        style: itemStyle,
+        ...itemProps
+      } = {},
+      gap = 8,
+      targetHeight = 300,
+      scaleFactor = 0.1,
+      layout: layoutType = "naive",
+      items = [],
+      renderItem,
+    }: Props,
+    ref
+  ) => {
+    const [viewportWidth, setViewportWidth] = React.useState<number>(0);
+    const containerRef = React.useRef<HTMLDivElement | null>(null);
 
-  const layout = React.useMemo(
-    () =>
-      calculateMasonryLayout({
-        viewportWidth,
-        items,
-        maxRowHeight: targetHeight * (1 + scaleFactor),
-      }),
-    [viewportWidth, items, targetHeight, scaleFactor]
-  );
+    const layout = React.useMemo(
+      () =>
+        layoutType === "naive"
+          ? calculateNaiveMasonryLayout({
+              viewportWidth,
+              items,
+              targetHeight,
+              scaleFactor,
+            })
+          : calculateJustifiedMasonryLayout({
+              viewportWidth,
+              items,
+              targetHeight,
+              scaleFactor,
+            }),
+      [layoutType, viewportWidth, items, targetHeight, scaleFactor]
+    );
 
-  // const layout = React.useMemo(
-  //   () =>
-  //     calculateMasonryLayout({
-  //       viewportWidth,
-  //       items,
-  //       targetHeight: 300,
-  //       scaleFactor: 0.1,
-  //     }),
-  //   [viewportWidth, items]
-  // );
+    React.useLayoutEffect(() => {
+      if (!containerRef.current || containerRef.current === null) return; // Return if the container is not found
+      let animationFrameID: number | null = null; // Initialize animation frame ID
 
-  React.useLayoutEffect(() => {
-    if (!containerRef.current || containerRef.current === null) return; // Return if the container is not found
-    let animationFrameID: number | null = null; // Initialize animation frame ID
+      const resizeObserver = new ResizeObserver((entries) => {
+        const newWidth = entries[0].contentRect.width; // This gives the actual width of the inside of the container
+        if (animationFrameID) cancelAnimationFrame(animationFrameID); // Cancel the previous animation frame
 
-    const resizeObserver = new ResizeObserver((entries) => {
-      const newWidth = entries[0].contentRect.width; // This gives the actual width of the inside of the container
-      if (animationFrameID) cancelAnimationFrame(animationFrameID); // Cancel the previous animation frame
-
-      animationFrameID = requestAnimationFrame(() => {
-        setViewportWidth(newWidth); // Update the viewport width based on the resize observer
+        animationFrameID = requestAnimationFrame(() => {
+          setViewportWidth(newWidth); // Update the viewport width based on the resize observer
+        });
       });
-    });
-    resizeObserver.observe(containerRef.current); // Observe the container for changes
-    return () => {
-      resizeObserver.disconnect(); // Cleanup the observer on unmount
-      if (animationFrameID) cancelAnimationFrame(animationFrameID); // Cancel the animation frame on unmount
-    };
-  }, [containerRef, gap, items, targetHeight, scaleFactor]);
+      resizeObserver.observe(containerRef.current); // Observe the container for changes
+      return () => {
+        resizeObserver.disconnect(); // Cleanup the observer on unmount
+        if (animationFrameID) cancelAnimationFrame(animationFrameID); // Cancel the animation frame on unmount
+      };
+    }, [containerRef, gap, items, targetHeight, scaleFactor]);
 
-  return (
-    <div
-      ref={containerRef}
-      className={cn("relative w-full flex flex-wrap", className)}
-      {...containerProps}
-    >
-      {items.map((item, index) => {
-        const measures = layout[0][index];
+    return (
+      <div
+        ref={mergeRefs(containerRef, ref)}
+        className={cn("relative w-full flex flex-wrap", className)}
+        {...containerProps}
+      >
+        {items.map((item, index) => {
+          const measures = layout[0][index];
 
-        return (
-          <div
-            key={index}
-            className={cn("absolute", itemClassName)}
-            style={{
-              top: measures.top,
-              left: measures.left,
-              width: measures.width,
-              height: measures.height,
+          return (
+            <div
+              key={index}
+              className={cn("absolute", itemClassName)}
+              style={{
+                top: measures.top,
+                left: measures.left,
+                width: measures.width,
+                height: measures.height,
 
-              paddingTop: measures.rowNo === 1 ? gap : gap / 2,
-              paddingBottom: measures.rowNo === layout[2] ? gap : gap / 2,
-              paddingLeft: measures.index === 0 ? gap : gap / 2,
-              paddingRight:
-                measures.index === measures.totalItemsInRow - 1 ? gap : gap / 2,
+                paddingTop: measures.rowNo === 1 ? gap : gap / 2,
+                paddingBottom: measures.rowNo === layout[2] ? gap : gap / 2,
+                paddingLeft: measures.index === 0 ? gap : gap / 2,
+                paddingRight:
+                  measures.index === measures.totalItemsInRow - 1
+                    ? gap
+                    : gap / 2,
 
-              ...itemStyle,
-            }}
-            {...itemProps}
-          >
-            {renderItem ? renderItem(item, index, measures) : null}
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+                ...itemStyle,
+              }}
+              {...itemProps}
+            >
+              {renderItem ? renderItem(item, index, measures) : null}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+);
+MasonryRowGrid.displayName = "MasonryRowGrid";
 
-// function calculateMasonryLayout({
-//   viewportWidth,
-//   items,
-//   maxRowHeight,
-// }: {
-//   viewportWidth: number;
-//   items: GridItemProps[];
-//   maxRowHeight: number;
-// }) {
-//   let rows: Measures[] = [];
-//   let row: Measures[] = [];
-//   let currentWidth = 0;
-//   let totalHeight = 0; // Initialize total height
-//   let rowNumber = 1; // Initialize row number
-
-//   function finalizeRow() {
-//     // Calculate the height of the row based on the width and max height
-//     const height =
-//       viewportWidth < currentWidth
-//         ? (viewportWidth / currentWidth) * maxRowHeight
-//         : maxRowHeight;
-
-//     let left = 0;
-//     for (let i = 0; i < row.length; i++) {
-//       const item = row[i];
-//       const width = (height / item.height) * item.width;
-
-//       row[i].width = width;
-//       row[i].height = height;
-//       row[i].top = totalHeight;
-//       row[i].left = left;
-
-//       row[i].index = i; // Update index in the row
-//       row[i].totalImagesInRow = row.length; // Update total images in the row
-//       left += width;
-//     }
-
-//     rows = rows.concat(row);
-//     totalHeight += height;
-//     row = [];
-//     currentWidth = 0;
-//     rowNumber += 1; // Increment row number
-//   }
-
-//   items.forEach((item) => {
-//     row.push({
-//       width: item.width,
-//       height: item.height,
-//       top: 0,
-//       left: 0,
-
-//       index: 0, // Placeholder, will be updated later
-//       rowNo: rowNumber,
-//       totalImagesInRow: 0, // Placeholder, will be updated later
-//     });
-//     currentWidth += (maxRowHeight / item.height) * item.width;
-
-//     if (currentWidth >= viewportWidth) finalizeRow();
-//   });
-
-//   if (row.length > 0) finalizeRow();
-
-//   return [rows, totalHeight, rowNumber] as const;
-// }
+export { MasonryRowGrid };
